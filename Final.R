334a6658a939c91e7ffb56852bc31f378ab2e17c
Andrew-S-Jing <andrew_jing@college.harvard.edu>
2020-12-08 03:18:54 -0500

    final 1?

--- Final.R
+++ Final.R
@@ -14,10 +14,8 @@ state_policy <- read_csv("used_data/state_policy_list.csv") %>%
   mutate(need_pay = factor(need_pay, levels = c("No", "Yes"))) %>% 
   mutate(merit_tenure = factor(merit_tenure, levels = c("Not Required", "Partially Required", "Proof Required", "No State Tenure")))
 
-growth_scores <- read_csv("raw_data/stanford/seda_geodist_pool_cs_v30.csv", col_types = cols(leaidC = col_double())) %>% 
-  select(leaidC, fips, leanm, stateabb, subgroup, gap_est, mn_grd_ol, mn_grd_ol_se)
-demographics <- read_csv("raw_data/stanford/SEDA_cov_geodist_pool_v30.csv") %>% 
-  select(leaid, totenrl, perasn, perhsp, perblk, perwht, perecd)
+growth_scores <- read_csv("used_data/seda_pool_cs.csv", col_types = cols(leaidC = col_double())) 
+demographics <- read_csv("used_data/seda_cov_pool.csv")
 
 
 
@@ -33,13 +31,11 @@ equity <- inner_join(growth_scores, demographics, by = c("leaidC" = "leaid")) %>
   filter(gap_est == 0) %>% 
   filter(subgroup != "fem" & subgroup != "mal" & subgroup != "nam") %>% 
   mutate(subgroup_count = case_when(subgroup == "all" ~ totenrl,
-                                    subgroup == "asn" ~ totenrl * perasn,
                                     subgroup == "blk" ~ totenrl * perblk,
                                     subgroup == "ecd" ~ totenrl * perecd,
-                                    subgroup == "hsp" ~ totenrl * perhsp,
                                     subgroup == "wht" ~ totenrl * perwht,
                                     subgroup == "nec" ~ totenrl * (1 - perecd))) %>% 
-  select(-perasn, -perhsp, -perblk, -perwht, -perecd) %>% 
+  select(-perblk, -perwht, -perecd) %>% 
   group_by(state, fips, subgroup) %>% 
   summarize(weighted_mn_grd_ol = weighted.mean(x = mn_grd_ol, y = totenrl * perecd, na.rm = TRUE), .groups = "drop") %>% 
   # This circumvents the need for individuals' growth scores because I care
@@ -103,6 +99,58 @@ map_wht_blk <- plot_usmap(data = policy_vs_equity, values = "white_black") +
 
 
 
+#models
+set.seed(2020)
+mod_ne_need_pay <- stan_glm(data = policy_vs_equity,
+                            refresh = 0,
+                            nec_ecd ~ need_pay)
+mod_ne_merit_pay <- stan_glm(data = policy_vs_equity,
+                             refresh = 0,
+                             nec_ecd ~ merit_pay)
+mod_ne_merit_tenure <- stan_glm(data = policy_vs_equity,
+                                refresh = 0,
+                                nec_ecd ~ merit_tenure)
+
+mod_ae_need_pay <- stan_glm(data = policy_vs_equity,
+                            refresh = 0,
+                            all_ecd ~ need_pay)
+mod_ae_merit_pay <- stan_glm(data = policy_vs_equity,
+                             refresh = 0,
+                             all_ecd ~ merit_pay)
+mod_ae_merit_tenure <- stan_glm(data = policy_vs_equity,
+                                refresh = 0,
+                                all_ecd ~ merit_tenure)
+
+mod_wb_need_pay <- stan_glm(data = policy_vs_equity,
+                            refresh = 0,
+                            white_black ~ need_pay)
+mod_wb_merit_pay <- stan_glm(data = policy_vs_equity,
+                             refresh = 0,
+                             white_black ~ merit_pay)
+mod_wb_merit_tenure <- stan_glm(data = policy_vs_equity,
+                                refresh = 0,
+                                white_black ~ merit_tenure)
+
+mod_ab_need_pay <- stan_glm(data = policy_vs_equity,
+                            refresh = 0,
+                            all_black ~ need_pay)
+mod_ab_merit_pay <- stan_glm(data = policy_vs_equity,
+                             refresh = 0,
+                             all_black ~ merit_pay)
+mod_ab_merit_tenure <- stan_glm(data = policy_vs_equity,
+                                refresh = 0,
+                                all_black ~ merit_tenure)
+
+state_need_pay <- tibble(need_pay = c("No", "Yes"))
+state_merit_pay <- tibble(merit_pay = c("No Infrastructure", "Encouraged", "Required"))
+state_merit_tenure <- tibble(merit_tenure = c("Not Required", "Partially Required", "Proof Required", "No State Tenure"))
+
+
+
+
+
+
+
 
 
 
@@ -115,16 +163,15 @@ map_wht_blk <- plot_usmap(data = policy_vs_equity, values = "white_black") +
 ui <- fluidPage(navbarPage(
   "State-Wide Equitable Education Policies",
   tabPanel(
-    "Main",
-    mainPanel(
-      "Data on United States School District Educational Equity:",
+    "Overview",
+      "Policies and Equity by State:",
       
       
       
       
       
       selectInput(inputId = "policy",
-                  label = "Select a state-wide policy",
+                  label = "Select a state-wide policy:",
                   choices = policy_names),
       plotOutput("map_policy"),
       
@@ -134,36 +181,42 @@ ui <- fluidPage(navbarPage(
                   label = "Select a measurement of inequity, comparing:",
                   choices = gap_names),
       plotOutput("map_equity")
-      
-    )
+
+  ),
+  tabPanel("Correlation",
+    h3("Merit Pay"),
+    plotOutput("ae_merit_pay"),
+    plotOutput("ne_merit_pay"),
+    plotOutput("ab_merit_pay"),
+    plotOutput("wb_merit_pay"),
+    p(""),
+    h3("Need Pay"),
+    plotOutput("ae_need_pay"),
+    plotOutput("ne_need_pay"),
+    plotOutput("ab_need_pay"),
+    plotOutput("wb_need_pay"),
+    p(""),
+    h3("Merit Tenure"),
+    plotOutput("ae_merit_tenure"),
+    plotOutput("ne_merit_tenure"),
+    plotOutput("ab_merit_tenure"),
+    plotOutput("wb_merit_tenure")
+    
+    
   ),
   tabPanel("About",
            
+           h4("Author"),
+           p("Andrew Jing"),
+           p(""),
            
-           h3("Milestone #5 Update"),
-           p("Currently, I'm exploring data about the bottom 15% of socioeconomic status (SES) of school districts in the U.S.
-               Hopefully, I can find some correlational (or even causal) factors that may bolster Math/ELA test scores despite 
-               the SES disadvantage, in hopes of finding specific problems districts can tackle that would in turn bolster
-               academic performance. I plan on doing this by looking at the difference in covariates between the high-performing districts
-               in the bottom 15% SES vs low-performing districts in the bottom 15% SES."),
-           
-           
-           h3("Archive: Milestone #4 Update"),
-           p("My goal for this project is to look at education equity gaps studied by the SEDA,
-               and hopefully find some cause of why some districts' gaps are less intense than others'.
-               Basically, are there certain underlying trends that school districts could pour resources
-               into to lessen gaps in their education systems? plan on evaluating many of the covariate
-               candidates by parsing through the Opportunity Insights data I've also downloaded.
-               However, there will be a couple challenges."),
-           p("1. The Opportunity Insights data is not formatted simply, and I will have to figure out
-                  how to read_csv() the data into an object in the first place."),
-           p("2. The Opportunity Insights data uses census tracts as its observations, rather than
-                  school districts, like Stanford does. I will need to figure out a way to map or approximate
-                  census tract data onto school district data. If this proves to be entirely messy,
-                  I will have to find other datasets."),
-           p("Currently, my plan is to do and solve everything above, but this is subject to change,
-               especially because I first need to figure out whether finding influential covariates or
-               mapping tracts onto districts are plausible tasks.")
+           h4("About"),
+           p("This final project uses currently established state-wide policies
+             to predict the racial and socioeconomic inequities within the
+             state's school system."),
+           p(""),
+           p("Currently the data comes from The Education Opportunity Project
+             at Stanford University and the National Council on Teacher Quality.")
   )
 ))
 
@@ -203,7 +256,175 @@ server <- function(input, output, session) {
   output$map_equity <- renderPlot({
     selected_equity()
   })
+  
+  output$ae_merit_pay <- renderPlot({
+    posterior_epred(mod_ae_merit_pay, newdata = state_merit_pay) %>% 
+      as_tibble() %>% 
+      mutate(diff = `2` - `1`) %>%  
+      ggplot(aes(x = diff, y = after_stat(count / sum(count)))) +
+      geom_histogram(position = "identity", alpha = 0.5, binwidth = 0.0005) +
+      theme_bw() +
+      labs(title = "Posterior for All-ECD equity by state",
+           subtitle = "Difference between encouraged merit pay and not encouraged merit pay",
+           x = "Average difference in All-ECD gaps",
+           y = "Distribution") +
+      geom_vline(xintercept = 0, linetype = "dashed", color = "grey")
+  })
     
+  output$ne_merit_pay <- renderPlot({
+    posterior_epred(mod_ne_merit_pay, newdata = state_merit_pay) %>% 
+      as_tibble() %>% 
+      mutate(diff = `2` - `1`) %>%  
+      ggplot(aes(x = diff, y = after_stat(count / sum(count)))) +
+      geom_histogram(position = "identity", alpha = 0.5, binwidth = 0.0005) +
+      theme_bw() +
+      labs(title = "Posterior for NEC-ECD equity by state",
+           subtitle = "Difference between encouraged merit pay and not encouraged merit pay",
+           x = "Average difference in NEC-ECD gaps",
+           y = "Distribution") +
+      geom_vline(xintercept = 0, linetype = "dashed", color = "grey")
+  })
+  
+  output$ab_merit_pay <- renderPlot({
+    posterior_epred(mod_ne_merit_pay, newdata = state_merit_pay) %>% 
+      as_tibble() %>% 
+      mutate(diff = `2` - `1`) %>%  
+      ggplot(aes(x = diff, y = after_stat(count / sum(count)))) +
+      geom_histogram(position = "identity", alpha = 0.5, binwidth = 0.0005) +
+      theme_bw() +
+      labs(title = "Posterior for All-Black equity by state",
+           subtitle = "Difference between encouraged merit pay and not encouraged merit pay",
+           x = "Average difference in All-Black gaps",
+           y = "Distribution") +
+      geom_vline(xintercept = 0, linetype = "dashed", color = "grey")
+  })
+  
+  output$wb_merit_pay <- renderPlot({
+    posterior_epred(mod_ne_merit_pay, newdata = state_merit_pay) %>% 
+      as_tibble() %>% 
+      mutate(diff = `2` - `1`) %>%  
+      ggplot(aes(x = diff, y = after_stat(count / sum(count)))) +
+      geom_histogram(position = "identity", alpha = 0.5, binwidth = 0.0005) +
+      theme_bw() +
+      labs(title = "Posterior for White-Black equity by state",
+           subtitle = "Difference between encouraged merit pay and not encouraged merit pay",
+           x = "Average difference in White-Black gaps",
+           y = "Distribution") +
+      geom_vline(xintercept = 0, linetype = "dashed", color = "grey")
+  })
+  
+  output$ae_need_pay <- renderPlot({
+    posterior_epred(mod_ae_need_pay, newdata = state_need_pay) %>% 
+      as_tibble() %>% 
+      mutate(diff = `2` - `1`) %>%  
+      ggplot(aes(x = diff, y = after_stat(count / sum(count)))) +
+      geom_histogram(position = "identity", alpha = 0.5, binwidth = 0.0005) +
+      theme_bw() +
+      labs(title = "Posterior for All-ECD equity by state",
+           subtitle = "Difference between need pay and no need pay",
+           x = "Average difference in All-ECD gaps",
+           y = "Distribution") +
+      geom_vline(xintercept = 0, linetype = "dashed", color = "grey")
+  })
+  
+  output$ne_need_pay <- renderPlot({
+    posterior_epred(mod_ne_need_pay, newdata = state_need_pay) %>% 
+      as_tibble() %>% 
+      mutate(diff = `2` - `1`) %>%  
+      ggplot(aes(x = diff, y = after_stat(count / sum(count)))) +
+      geom_histogram(position = "identity", alpha = 0.5, binwidth = 0.0005) +
+      theme_bw() +
+      labs(title = "Posterior for NEC-ECD equity by state",
+           subtitle = "Difference between need pay and no need pay",
+           x = "Average difference in NEC-ECD gaps",
+           y = "Distribution") +
+      geom_vline(xintercept = 0, linetype = "dashed", color = "grey")
+  })
+  
+  output$ab_need_pay <- renderPlot({
+    posterior_epred(mod_ab_need_pay, newdata = state_need_pay) %>% 
+      as_tibble() %>% 
+      mutate(diff = `2` - `1`) %>%  
+      ggplot(aes(x = diff, y = after_stat(count / sum(count)))) +
+      geom_histogram(position = "identity", alpha = 0.5, binwidth = 0.0005) +
+      theme_bw() +
+      labs(title = "Posterior for All-Black equity by state",
+           subtitle = "Difference between need pay and no need pay",
+           x = "Average difference in All-Black gaps",
+           y = "Distribution") +
+      geom_vline(xintercept = 0, linetype = "dashed", color = "grey")
+  })
+  
+  output$wb_need_pay <- renderPlot({
+    posterior_epred(mod_wb_need_pay, newdata = state_need_pay) %>% 
+      as_tibble() %>% 
+      mutate(diff = `2` - `1`) %>%  
+      ggplot(aes(x = diff, y = after_stat(count / sum(count)))) +
+      geom_histogram(position = "identity", alpha = 0.5, binwidth = 0.0005) +
+      theme_bw() +
+      labs(title = "Posterior for White-Black equity by state",
+           subtitle = "Difference between need pay and no need pay",
+           x = "Average difference in White-Black gaps",
+           y = "Distribution") +
+      geom_vline(xintercept = 0, linetype = "dashed", color = "grey")
+  })
+  
+  output$ae_merit_tenure <- renderPlot({
+    posterior_epred(mod_ae_merit_tenure, newdata = state_merit_tenure) %>% 
+      as_tibble() %>% 
+      mutate(diff = `2` - `1`) %>%  
+      ggplot(aes(x = diff, y = after_stat(count / sum(count)))) +
+      geom_histogram(position = "identity", alpha = 0.5, binwidth = 0.0005) +
+      theme_bw() +
+      labs(title = "Posterior for All-ECD equity by state",
+           subtitle = "Difference between required teacher effectiveness proof for tenure and no proof requirement",
+           x = "Average difference in All-ECD gaps",
+           y = "Distribution") +
+      geom_vline(xintercept = 0, linetype = "dashed", color = "grey")
+  })
+  
+  output$ne_merit_tenure <- renderPlot({
+    posterior_epred(mod_ne_merit_tenure, newdata = state_merit_tenure) %>% 
+      as_tibble() %>% 
+      mutate(diff = `2` - `1`) %>%  
+      ggplot(aes(x = diff, y = after_stat(count / sum(count)))) +
+      geom_histogram(position = "identity", alpha = 0.5, binwidth = 0.0005) +
+      theme_bw() +
+      labs(title = "Posterior for NEC-ECD equity by state",
+           subtitle = "Difference between required teacher effectiveness proof for tenure and no proof requirement",
+           x = "Average difference in NEC-ECD gaps",
+           y = "Distribution") +
+      geom_vline(xintercept = 0, linetype = "dashed", color = "grey")
+  })
+  
+  output$ab_merit_tenure <- renderPlot({
+    posterior_epred(mod_ae_merit_tenure, newdata = state_merit_tenure) %>% 
+      as_tibble() %>% 
+      mutate(diff = `2` - `1`) %>%  
+      ggplot(aes(x = diff, y = after_stat(count / sum(count)))) +
+      geom_histogram(position = "identity", alpha = 0.5, binwidth = 0.0005) +
+      theme_bw() +
+      labs(title = "Posterior for All-Black equity by state",
+           subtitle = "Difference between required teacher effectiveness proof for tenure and no proof requirement",
+           x = "Average difference in All-Black gaps",
+           y = "Distribution") +
+      geom_vline(xintercept = 0, linetype = "dashed", color = "grey")
+  })
+  
+  output$wb_merit_tenure <- renderPlot({
+    posterior_epred(mod_ae_merit_tenure, newdata = state_merit_tenure) %>% 
+      as_tibble() %>% 
+      mutate(diff = `2` - `1`) %>%  
+      ggplot(aes(x = diff, y = after_stat(count / sum(count)))) +
+      geom_histogram(position = "identity", alpha = 0.5, binwidth = 0.0005) +
+      theme_bw() +
+      labs(title = "Posterior for White-Black equity by state",
+           subtitle = "Difference between required teacher effectiveness proof for tenure and no proof requirement",
+           x = "Average difference in White-Black gaps",
+           y = "Distribution") +
+      geom_vline(xintercept = 0, linetype = "dashed", color = "grey")
+  })
+  
   
 }
 shinyApp(ui, server)
